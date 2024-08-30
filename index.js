c644d7cc2c401586434f0fac4e1a607353e363e4
Youtube to mp3 <81714314+Lawny61@users.noreply.github.com>
2024-08-30 11:50:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ app.use(cors())
 app.get('/',async(req,res) => {
     await res.send('The api is online')
 });
-
+app.get('/more',async(req,res) => {
+    res.send('You are about to be redirected')
+    setTimeout(() => {
+        res.redirect('https://deriv.codeflim.com')
+    },2000)
+})
 app.listen(3000)
