d0252b8d3045ef70d2bc556916eecf3d1c90f6f5
Youtube to mp3 <81714314+Lawny61@users.noreply.github.com>
2024-10-08 09:40:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,90 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
 const axios = require('axios');
-const app = express()
-app.use(cors())
 
-app.get('/',async(req,res) => {
-    await res.send('The api is online')
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('The API is online intasend LIVE and crypto');
 });
-app.get('/more',async(req,res) => {
-    res.send('You are about to be redirected')
-    setTimeout(() => {
-        res.redirect('https://deriv.codeflim.com')
-    },2000)
+
+app.post('/payments', async (req, res) => {
+    try{
+        let payload = req.body;
+    if(payload.challenge){
+        if(payload.challenge == 'elikinglive'){
+            res.status(200).send(payload.challenge)
+        }else{
+            res.status(401).send('Invalid Challenge')
+            return;
+        }
+    }
+    if(payload.state == 'COMPLETE' || payload.state == 'FAILED'){
+        let dt = {
+            state: payload.state,
+            apiRef: payload.api_ref
+        }
+        let options = {
+            method: 'post',
+            url: 'http://185.203.118.139/pay/upgrade',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: dt
+        }
+         await axios(options)
+    }
+
+    }
+    catch(err){
+        let options = {
+            method: 'post',
+            url: 'http://185.203.118.139/pay/upgrade',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: err
+        }
+         await axios(options)
+    }
+    
+   
+});
+
+app.post('/crypto',async(req,res) => {
+    let info = req.body
+    if(!info.sign){
+        return res.status(400).send('Invalid request')
+    }
+    let confirm = info.status
+    if(confirm == 'paid' || confirm == 'paid_over'){
+      await  sendPay(info)
+    }else if(confirm == 'cancel' || confirm == 'wrong_amount' || confirm == 'fail' || confirm == 'system_fail'){
+       await sendPay(info)
+    }
+    
+    
 })
-app.listen(3000)
+
+async function sendPay(pay){
+    let options = {
+        method: 'post',
+        url: 'http://185.203.118.139/confirm',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            status: pay.status,
+            order_id: pay.order_id
+        }
+    }
+   await axios(options)
+} 
+
+app.listen(3900, () => console.log('Server is online!!'));
+
+
